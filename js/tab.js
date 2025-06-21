c0db8195bf38bc94002636fbc45816e7389bc905
truongvd05 <truongvdf8@fullstack.edu.vn>
2025-06-21 16:12:19 +0700

    add url params

--- js/tab.js
+++ js/tab.js
@@ -1,23 +1,31 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const wrap = $(".wrap");
 const tabs = $$(".tab");
 const content = $$(".content-tab");
 const body = document.body;
 
-// console.log(content);
-
-// active mặc định khi tải trang
-tabs.forEach((item, index) => {
-    if (item.classList.contains("active")) {
-        addAtribute();
-        content[index].removeAttribute("hidden");
-    }
-});
-
-// add active
+const params = new URLSearchParams(location.search);
+const tabId = wrap.id;
+const tabIndex = params.get(tabId) ?? 0;
+// activate default on the first page load
+if (params) {
+    tabs[tabIndex].classList.add("active");
+    console.log(tabIndex);
+}
+// add active + URLsearchParams
 tabs.forEach((item, index) => {
     item.onclick = function () {
+        if (index) {
+            params.set(tabId, index);
+        } else {
+            params.delete(tabId);
+        }
+        const Url = params.size ? `?${params}` : "";
+        const saveUrl = `${location.pathname}${Url}${location.hash}`;
+        history.replaceState(null, null, saveUrl);
+
         removeClass();
         item.classList.add("active");
         if (item.classList.contains("active")) {
